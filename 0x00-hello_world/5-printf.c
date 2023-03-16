b232f166984e5d16ff344c65e9136ef7c5d94953
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 09:23:34 -0700

    prints sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - function return a void
+* Description: prints with proper grammar, but the outcome is a piece of art,
+* printf: outputs the sentence to the stdout
+* Return: 0 value if there is no error
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
