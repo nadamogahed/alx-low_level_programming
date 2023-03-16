b21827fb0752b602e0362711bc8c9bdad2c1304a
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 08:35:03 -0700

    prints sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /* prints a sentence */
 int main(void)
 {
-	/*
-	 * Description: main() function return a void 
-	 * because we are printing a string value
-	 * strcpy()copy the sentence into str1 
-	 * puts() outputs the sentence to the stdout.
-	 */
+/*
+ * main() function return a void 
+ * because we are printing a string value
+ * strcpy()copy the sentence into str1 
+ * puts() outputs the sentence to the stdout.
+ */
 char str1[100];
 strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
