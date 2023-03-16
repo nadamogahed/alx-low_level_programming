b3de50faf071a0b656ea6dbee91b1918f6fd64a0
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 07:54:41 -0700

    prints sentance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 int main () {
    char str1[100];
 
-   strcpy(str1, "Programming is like building a multilingual puzzle");
+   strcpy(str1,"\"Programming is like building a multilingual puzzle");
 
    puts(str1);
 
