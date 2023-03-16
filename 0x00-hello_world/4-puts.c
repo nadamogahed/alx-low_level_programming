76f94565bc86180b90edd78a2c349c959225b90e
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 08:07:43 -0700

    prints sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void) {
-/* prints a sentence */
+int main(void) {
+/* 
+ * prints a sentence 
+ */
 char str1[100];
-strcpy(str1,"\"Programming is like building a multilingual puzzle");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
-return(0);
+return (0);
 }
