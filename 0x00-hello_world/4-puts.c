8d6d37cc299ae7866545f19a9852e5146be4a1d6
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 08:02:43 -0700

    prints sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,8 @@
 
 int main (void) {
 /* prints a sentence */
-   char str1[100];
-
-   strcpy(str1,"\"Programming is like building a multilingual puzzle");
-
-   puts(str1);
-
-   return(0);
+char str1[100];
+strcpy(str1,"\"Programming is like building a multilingual puzzle");
+puts(str1);
+return(0);
 }
