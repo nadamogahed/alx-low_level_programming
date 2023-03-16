dd42c37f8dd60cbd2745a3914e1127021224b731
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 08:45:48 -0700

    prints sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-/**
-* main - function return a void 
+/* main - function return a void 
 * because we are printing a string value
-* strcpy()copy the sentence into str1 
+* strcpy() copy the sentence into str1 
 * puts() outputs the sentence to the stdout.
-* return - a 0 value if there is no error
-*/
+* return: 0 value if there is no error*/
 int main(void)
 {
 char str1[100];
