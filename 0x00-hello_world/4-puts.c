5086961507a8f494700feab939eecc0a06169b5d
nadamogahed <nadamarcy25@gmail.com>
2023-03-16 09:01:01 -0700

    prints sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* main:function return a void
-* strcpy:copy the sentence into str1
-* puts:outputs the sentence to the stdout
-* return:0 value if there is no error
+* main function return a void
+* char str[100] an array of characters
+* strcpy copy the sentence into str1
+* puts outputs the sentence to the stdout
+* return 0 value if there is no error
 */
 int main(void)
 {
