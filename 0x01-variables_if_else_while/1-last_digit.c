6881679b84936bb09b8c1492450e97c6d998fc7f
nadamogahed <nadamarcy25@gmail.com>
2023-03-17 19:43:11 -0700

    positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,16 +16,17 @@ int main(void)
 	/* your code goes there */
 	last_num = n % 10;
 	if (last_num > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
-        }
+	{
+		printf("Last digit of %d is %d and is greater than 5\n"
+								, n, last_num);
+	}
 	else if (last_num == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_num);
 	}
 	else
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
-        }
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
+	}
 	return (0);
 }
