8a160df5e66cbed11b89bfcb7de6a8db0bc545b7
nadamogahed <nadamarcy25@gmail.com>
2023-03-18 20:31:26 -0700

    single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers
+ * Description: prints the numbers from 0 to 9.
+ * Return: 0 if success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
