44f7d2c8933b42c9747f03ad19bf686ddb56666f
nadamogahed <nadamarcy25@gmail.com>
2023-03-24 15:09:50 -0700

    print the absoulte value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - prints the alphabet
+ * @c: single letter input.
+ * Return: 1 if c is lowercase
+*/
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i /=  -1;
+		_putchar(i);
+	}
+	else
+	{
+		_putchar(i);
+	}
+}
