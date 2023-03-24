d242167302e6a92988db0054d0240d879985a539
nadamogahed <nadamarcy25@gmail.com>
2023-03-24 13:27:57 -0700

    print the sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,21 @@
 #include "main.h"
 /**
  * print_sign - prints the alphabet
- * @c: single letter input.
- * Return: 1 if c is lowercase
+ * @c: single number input.
+ * Return: 0 if n is zero
 */
 int print_sign(int n)
 {
-
 	if (n > 0)
+	{
 		return (1);
+	}
 	else if	(n == 0)
+	{
 		return (0);
+	}
 	else (n < -1)
+	{
 		return (-1);
+	}
 }
