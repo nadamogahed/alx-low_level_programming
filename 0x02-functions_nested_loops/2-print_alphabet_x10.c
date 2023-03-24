0e7732137208acda0f6cb8ffa9b97344e9787d6d
nadamogahed <nadamarcy25@gmail.com>
2023-03-24 08:51:50 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ * Description: prints the alphabet in lowercase.
+ * Return: 0 if success
+*/
+void print_alphabet(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; c <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
