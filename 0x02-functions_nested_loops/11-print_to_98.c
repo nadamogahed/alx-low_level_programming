07f294a973e2f314d315ca5a11de3a88eb6a27a9
nadamogahed <nadamarcy25@gmail.com>
2023-03-26 03:58:36 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - prints to 98
+ * @n: is the input start number
+ * Description: prints the numbers from n to 98.
+ * Return: no return function
+*/
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+}
