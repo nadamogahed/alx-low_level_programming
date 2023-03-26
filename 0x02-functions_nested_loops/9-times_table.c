163367acb36b65ec6a28575e5c23d656f77fc81b
nadamogahed <nadamarcy25@gmail.com>
2023-03-25 17:52:56 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ * Description: prints the times table.
+ * Return: no return function
+*/
+void times_table(void)
+{
+	int i;
+	int x;
+	int sum;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (x = 0; x < 10; x++)
+		{
+			sum = (i * x);
+			if ((sum / 10) > 0)
+			{
+				_putchar((sum / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((sum % 10) + '0');
+
+			if (x < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
