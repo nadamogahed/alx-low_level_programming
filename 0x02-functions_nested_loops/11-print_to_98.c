77452221e5e00268461ea1748b347519efa4efe3
nadamogahed <nadamarcy25@gmail.com>
2023-03-26 04:14:23 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,21 @@
 */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n < 98)
 	{
 		printf("%d", n);
 		n++;
+		if (n == 98)
+			continue;
+		printf(", ");
 	}
 	while (n > 98)
 	{
 		printf("%d", n);
 		n--;
+		if (n == 98)
+			continue;
+		printf(", ");
 	}
 	printf("\n");
 }
