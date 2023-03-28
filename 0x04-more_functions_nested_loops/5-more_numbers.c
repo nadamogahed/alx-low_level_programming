9c909fc3fc143bf72c70f65a0813743c56530b55
nadamogahed <nadamarcy25@gmail.com>
2023-03-27 21:41:48 -0700

    print numbers to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_numbers - check if char is upper or not
+ * Return: no return value
+*/
+void print_numbers(void)
+{
+	int i, x;
+
+	while (i < 10)
+	{
+		for (x = 48; x < 15; x++)
+		{
+			_putchar(c);
+		}
+		_putchar(49);
+		_putchar(49);
+		_putchar(49);
+		_putchar(50);
+		_putchar(49);
+		_putchar(51);
+		_putchar(49);
+		_putchar(52);
+		_putchar('\n');
+		i++;
+	}
+}
