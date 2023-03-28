72d430c5c732b59ba5a724da7f387598a9b5d270
nadamogahed <nadamarcy25@gmail.com>
2023-03-27 21:23:47 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ * Return: no return value
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c == 50 && c == 52)
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
