8215c3b09e069f078c261ade9ca8248277b2e8a4
nadamogahed <nadamarcy25@gmail.com>
2023-03-28 14:04:55 -0700

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints the prints numbers.
+ * Return: 0 if success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
+
