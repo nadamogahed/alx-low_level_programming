b88f8b72219339cd24551373db1a9f04d75cd77d
nadamogahed <nadamarcy25@gmail.com>
2023-03-27 21:06:48 -0700

    from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check if char is upper or not
+ * @c: char input.
+ * Return: no return value
+*/
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+}
