1776e41e22db23d97bc2ff8e140596d61e4f257c
nadamogahed <nadamarcy25@gmail.com>
2023-03-29 15:34:26 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,8 +6,7 @@
 */
 void rev_string(char *s)
 {
-	int x;
-	int i = 0;
+	int i;
 	int len = 0;
 	char tmp;
 
@@ -16,14 +15,11 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (x = len; x != i; x--)
+	for (i = 0; i < len / 2; i++)
 	{
-		if (s[x] == '\0')
-			continue;
-		tmp = s[i];
-		s[i] = s[x];
-		s[x] = tmp;
-		i++;
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
 
