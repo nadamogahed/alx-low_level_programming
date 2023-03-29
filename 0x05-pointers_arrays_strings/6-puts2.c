26c66537ad1446899771db72c6636f5f31935f85
nadamogahed <nadamarcy25@gmail.com>
2023-03-29 16:04:01 -0700

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 -  swaps the values of two integers
+ * @str: input string
+ * Return: no return function
+*/
+void puts2(char *str)
+{
+	int i;
+
+	while (str[len] != '\0')
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ int _strlen(char *s);
 void _putchar(char);
 void _puts(char *str);
 void rev_string(char *s);
+void puts2(char *str);
 #endif
