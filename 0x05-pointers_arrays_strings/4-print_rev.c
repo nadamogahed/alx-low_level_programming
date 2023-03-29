2defd3ffd5016c545da3c758c9e097f7f02de94b
nadamogahed <nadamarcy25@gmail.com>
2023-03-29 12:04:12 -0700

    string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: array of chars
+ * Return: no return value
+*/
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		if (s[i] == '\0')
+			continue;
+		_putchar(s[i]);
+	}
+}
+
