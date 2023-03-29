03bb24e2133369a1af22dc1fb4a97f3932deca30
nadamogahed <nadamarcy25@gmail.com>
2023-03-29 10:55:32 -0700

    put a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check if its digit or char
+ * @c: input.
+ * Return: 1 if c is uppercase
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
