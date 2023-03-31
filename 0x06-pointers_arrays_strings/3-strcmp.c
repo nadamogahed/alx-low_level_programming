5ddc3936e36efda9c32b59dfc1a12072631bb145
nadamogahed <nadamarcy25@gmail.com>
2023-03-31 13:20:17 -0700

    diff between two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: string
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i, result = 0;
+
+	result = s1[0] - s2[0];
+
+	return (result);
+}
