6135b2ef6dbe097566b42e8b45d9faafffba61e3
nadamogahed <nadamarcy25@gmail.com>
2023-03-31 11:28:26 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatnate two strings
+ * @dest: input first string
+ * @src: input second string
+ * @n: input for when to stop
+ * Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
