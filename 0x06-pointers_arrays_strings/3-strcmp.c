a6906a4f0c371243bc485aaacda027f7ed26ff7b
nadamogahed <nadamarcy25@gmail.com>
2023-03-31 13:40:01 -0700

    diff between two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,14 @@
 */
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
 	int result = 0;
 
-	result = s1[0] - s2[0];
+	while (s1[i] != '\0')
+	{
+		result = s1[i] - s2[i];
+		i++;
+	}
 
 	return (result);
 }
