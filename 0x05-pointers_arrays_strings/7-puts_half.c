4b08f6fc468a01dedf0e79c70cc04318254211f4
nadamogahed <nadamarcy25@gmail.com>
2023-04-02 14:03:47 -0700

    second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,10 +25,10 @@ void puts_half(char *str)
 	else
 	{
 		n = len / 2;
-                for (i = n; i < len; i--)
-                {
-                        _putchar(str[i]);
-                }
+		for (i = n; i < len; i--)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
