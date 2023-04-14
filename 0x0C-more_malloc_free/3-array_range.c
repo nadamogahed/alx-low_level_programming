7bbef6336f45604f7ecd0ed972dbe30692cc9b1f
nadamogahed <nadamarcy25@gmail.com>
2023-04-14 13:53:54 -0700

    create an array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,19 +11,16 @@
 int *array_range(int min, int max)
 {
 	int *array_ptr;
-	int i = 0, size = max - min + 1;
+	int size = max - min + 1;
+	int i;
 
 	if (min > max)
 		return (NULL);
 
 	array_ptr = malloc(size * sizeof(int));
 
-	while (min <= max)
-	{
-		array_ptr[i] = min;
-		i++;
-		min++;
-	}
+	for (i = 0; i < size; i++)
+		array_ptr[i] = min + i;
 
 	return (array_ptr);
 }
