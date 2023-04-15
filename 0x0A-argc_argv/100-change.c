ea03e59b955e00b891c67de820e22c59f96b3482
nadamogahed <nadamarcy25@gmail.com>
2023-04-14 22:54:12 -0700

    coins to make change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+	coins = 0;
+
+	if (cents > 0)
+	{
+		coins += cents / 25;
+		cents %= 25;
+
+		coins += cents / 10;
+		cents %= 10;
+
+		coins += cents / 5;
+		cents %= 5;
+
+		coins += cents / 2;
+		cents %= 2;
+
+		coins += cents;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
