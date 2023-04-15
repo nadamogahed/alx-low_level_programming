07bbab9e7c7a405c3ef9afaf57776adc9c6929a6
nadamogahed <nadamarcy25@gmail.com>
2023-04-14 22:42:19 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,10 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
+	int a, b;
+
 	if (argc == 3)
 	{
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 	}
 	else
