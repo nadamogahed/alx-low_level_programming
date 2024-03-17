ac43d7f26297da40468cfb5f647d1a04a47bfeae
nadamogahed <nadamarcy25@gmail.com>
2024-03-17 16:34:18 -0700

    task3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,39 +12,39 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
-    dlistint_t *last = *head;  /* used in step 5*/
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;  /* used in step 5*/
 
-    /* 1. allocate node */
-    if (new_node == NULL)
-        return (NULL);
+	/* 1. allocate node */
+	if (new_node == NULL)
+		return (NULL);
 
-    /* 2. put in the data  */
-    new_node->n = n;
+	/* 2. put in the data  */
+	new_node->n = n;
 
-    /* 3. This new node is going to be the last node, so
+	/* 3. This new node is going to be the last node, so
           make next of it as NULL*/
-    new_node->next = NULL;
+	new_node->next = NULL;
 
-    /* 4. If the Linked List is empty, then make the new
+	/* 4. If the Linked List is empty, then make the new
           node as head */
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-        last = last->next;
+	/* 5. Else traverse till the last node */
+	while (last->next != NULL)
+		last = last->next;
 
-    /* 6. Change the next of last node */
-    last->next = new_node;
+	/* 6. Change the next of last node */
+	last->next = new_node;
 
-    /* 7. Make last node as previous of new node */
-    new_node->prev = last;
+	/* 7. Make last node as previous of new node */
+	new_node->prev = last;
 
-    return (new_node);
+	return (new_node);
 }
 
